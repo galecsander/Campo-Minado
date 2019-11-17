5f1cc4e1cf0ec2577f7598f561c0e728c2d917cc
galecsander <57504506+galecsander@users.noreply.github.com>
2019-11-17 10:38:57 -0300

    Arrumei o códidgo de impressão da matriz m1
    Problema:
              estava sendo feita uma série e impresões no formato da matriz.
    
    Pós Correção:
             agora os valores da matriz m1 são todos convertidos para "0" e 
             depois disso a matriz é impressa.

--- eai.c
+++ eai.c
@@ -6,24 +6,33 @@ void preencheMatriz(){
   int i=0;
   int j=0;
 
+
     int mat1 [12][12];
     int mat2 [12][12];
+
     int zero=0;
     
     //preencher matriz m1;
+
     for(i=0;i<9;i++){
           for(j=0;j<10;j++){
               scanf("%d",&mat1[i][j]);
           }
-    }
+    } 
 
-     //imprimir matriz m1;
+     //imprimir matriz m1 com zero "0" em todas suas posições;
+
      for(i=0;i<9;i++){
           for(j=0;j<10;j++){
-              printf("%d ",zero); // cria matriz com zero "0" em todas suas posições;
+            if (mat1[i][j]>=0){
+                 mat1[i][j]=0;
+            } 
+               printf("%d ",mat1[i][j]);
           }printf("\n");
-    }
-}
+          }
+
+  
+     }
 
 
 
